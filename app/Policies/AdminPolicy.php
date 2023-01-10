e0fb9b1e6a2f9e3e2ccdaa8913401af028cd09b2
Leopalds <pedro.leopoldo@estudante.ufjf.br>
2023-01-10 01:08:23 -0300

    Policies quase funcionando

--- app/Policies/AdminPolicy.php
+++ app/Policies/AdminPolicy.php
@@ -2,11 +2,11 @@
 
 namespace App\Policies;
 
-use App\Models\Equipament;
+use App\Models\Admin;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-class EquipamentPolicy
+class AdminPolicy
 {
     use HandlesAuthorization;
 
@@ -18,19 +18,19 @@ class EquipamentPolicy
      */
     public function viewAny(User $user)
     {
-        return User::ADMIN == $user->type || User::PERSONAL == $user->type;
+        return true;
     }
 
     /**
      * Determine whether the user can view the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\Equipament  $equipament
+     * @param  \App\Models\Admin  $admin
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function view(User $user, Equipament $equipament)
+    public function view(User $user, Admin $admin)
     {
-        return User::ADMIN == $user->type || User::PERSONAL == $user->type;
+        return User::ADMIN == $user->type;
     }
 
     /**
@@ -41,29 +41,29 @@ class EquipamentPolicy
      */
     public function create(User $user)
     {
-        return User::ADMIN == $user->type || User::PERSONAL == $user->type;
+        return User::ADMIN == $user->type;
     }
 
     /**
      * Determine whether the user can update the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\Equipament  $equipament
+     * @param  \App\Models\Admin  $admin
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function update(User $user, Equipament $equipament)
+    public function update(User $user, Admin $admin)
     {
-        return User::ADMIN == $user->type || User::PERSONAL == $user->type;
+        return User::ADMIN == $user->type;
     }
 
     /**
      * Determine whether the user can delete the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\Equipament  $equipament
+     * @param  \App\Models\Admin  $admin
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function delete(User $user, Equipament $equipament)
+    public function delete(User $user, Admin $admin)
     {
         return User::ADMIN == $user->type;
     }
@@ -72,12 +72,11 @@ class EquipamentPolicy
      * Determine whether the user can restore the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\Equipament  $equipament
+     * @param  \App\Models\Admin  $admin
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function restore(User $user, Equipament $equipament)
+    public function restore(User $user, Admin $admin)
     {
-        return true;
         //
     }
 
@@ -85,12 +84,11 @@ class EquipamentPolicy
      * Determine whether the user can permanently delete the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\Equipament  $equipament
+     * @param  \App\Models\Admin  $admin
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function forceDelete(User $user, Equipament $equipament)
+    public function forceDelete(User $user, Admin $admin)
     {
-        return true;
         //
     }
 }
