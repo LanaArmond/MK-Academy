be3b68eacdd6e185fa393bb0f976929d41940fba
Leopalds <pedro.leopoldo@estudante.ufjf.br>
2023-01-09 22:28:37 -0300

    arrumando login image

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
         <x-slot name="logo">
             <a href="/">
                 {{-- <x-application-logo class="w-20 h-20 fill-current text-gray-500" /> --}}
-                <img src="{{ public_path("/img/Logo.png") }}" />
+                <img src="{{ asset("/img/Logo.png") }}" />
             </a>
         </x-slot>
 
--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 
 Route::middleware('auth', 'pendent')->group(function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    });
+
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->middleware(['auth', 'verified'])->name('dashboard');
